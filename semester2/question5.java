6000443c61f49179592a8dd227651806d754a331
Akshat Jha <akshatjha3125@gmail.com>
2025-03-02 01:10:59 +0530

    added question5

--- semester2/question5.java
+++ semester2/question5.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 public class question5 {
-    public static int sum_of_digits(int n){
-        if(n<10){
+    public static int sum_of_digits(int n) {
+        if (n < 10) {
             return n;
         }
-        int sum=0;
-        while(n>0){
-            sum+=n%10;
-            n=n/10;
-            }
-        return sum_of_digits(sum);
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n = n / 10;
         }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        return sum_of_digits(sum);
+    }
 
-        System.out.println("sum of the digits "+n+" until the number is a single digit is "+sum_of_digits(n));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("sum of the digits " + n + " until the number is a single digit is " + sum_of_digits(n));
     }
-    }
+}
 
 
